15eea6f4f22ae45bc993ce1b22727b09ea6a3223
Mike Anderson <hello@mrmikea.com>
2014-10-09 11:17:20 -0500

    Page rendering error messages

--- lib/wax/page_renderer.rb
+++ lib/wax/page_renderer.rb
@@ -8,12 +8,19 @@ module Wax
     end
 
     def render(page)
-      content = page.modules.map do |mod|
-        template = File.join(@paths[:templates], mod.template) + ".mustache"
-        data     = File.join(@paths[:data], mod.data) + ".json"
-        Mustache.render File.read(template), JSON.parse(File.read(data))
+      begin
+        puts "-> Rendering page: #{page.name}"
+        content = page.modules.map do |mod|
+          template = File.join(@paths[:templates], mod.template) + ".mustache"
+          data     = File.join(@paths[:data], mod.data) + ".json"
+          puts mod.template + "..."
+          Mustache.render File.read(template), JSON.parse(File.read(data, encoding: "bom|utf-8"))
+        end
+        Mustache.render File.read(@paths[:layout]), content: content.join("")
+      rescue => e
+        puts "Error rendering page: #{e}"
+        puts e.backtrace
       end
-      Mustache.render File.read(@paths[:layout]), content: content.join("")
     end
   end
 end
